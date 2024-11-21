ba117bd45bb9f0f1982ef71190579a1b79fd8c7e
Nicolas Ramirez <nramirez@theknotww.com>
2024-11-21 12:52:04 +0100

    add markdown plugin

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,13 @@
+return {
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+    vim.g.mkdp_echo_preview_url = 1
+  end,
+  keys = {
+    { '<leader>om', '<cmd>MarkdownPreview<cr>', desc = 'Markdown Preview' },
+  },
+  ft = { 'markdown' },
+}
