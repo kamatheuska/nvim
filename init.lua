62c3c3ac463743b2ffcf84a907e2a04d4eab90fb
Nicolas Ramirez <nramirez@theknotww.com>
2025-02-04 12:01:37 +0100

    disable unwanted providers

--- init.lua
+++ init.lua
@@ -98,6 +98,11 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 
+-- disable unwanted providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
