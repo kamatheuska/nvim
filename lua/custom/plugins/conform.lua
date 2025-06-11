e7e37d2e048e62eb6c84421ec30007b95fef3780
Nicolas Ramirez <nicolas@matizlab.com>
2025-06-11 10:55:56 +0200

    latest changes

---
+++ lua/custom/plugins/conform.lua
@@ -0,0 +1,71 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  config = function()
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat-on-save',
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Re-enable autoformat-on-save',
+    })
+
+    require('conform').setup {
+      log_level = vim.log.levels.DEBUG,
+      notify_on_error = true,
+      formatters_by_ft = {
+        lua = { 'stylua' },
+        -- Conform can also run multiple formatters sequentially
+        -- python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        yaml = { 'prettier' },
+        markdown = { 'prettier' },
+        graphql = { 'prettier' },
+      },
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+    }
+  end,
+  lazy = false,
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+}
--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -12,11 +12,11 @@ return {
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end)
-    vim.keymap.set('n', '<C-e>', function()
+    vim.keymap.set('n', '<leader>e', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
-    vim.keymap.set('n', '<C-g>', function()
+    vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(1)
     end)
     vim.keymap.set('n', '<C-t>', function()
