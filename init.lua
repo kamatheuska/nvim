0dab6826823153a67855300dca0191648ea5da1b
Nicolas Ramirez <nramirez@theknotww.com>
2025-06-11 11:00:41 +0200

    keys

--- init.lua
+++ init.lua
@@ -412,6 +412,7 @@ require('lazy').setup({
       -- See `:help telescope` and `:help telescope.setup()`
 
       local lga_actions = require 'telescope-live-grep-args.actions'
+      local actions = require 'telescope.actions'
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
@@ -425,6 +426,12 @@ require('lazy').setup({
         pickers = {
           find_files = {
             find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+          },
+          buffers = {
+            mappings = {
+              i = { ['<c-d>'] = actions.delete_buffer },
+              n = { ['<c-d>'] = actions.delete_buffer },
+            },
           },
         },
         defaults = {
@@ -494,6 +501,7 @@ require('lazy').setup({
       end, { desc = '[S]earch [N]eovim files' })
     end,
   },
+  { 'towolf/vim-helm', ft = 'helm' },
 
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
@@ -718,6 +726,12 @@ require('lazy').setup({
             filetypes = { 'yaml.docker-compose', 'yml.docker-compose' },
           },
         },
+        ['helm-ls'] = {
+          yamlls = {
+            path = 'yaml-language-server',
+          },
+        },
+        yamlls = {},
         dockerls = {},
         lua_ls = {
           -- cmd = {...},
@@ -1037,6 +1051,71 @@ require('lazy').setup({
   {
     'arcticicestudio/nord-vim',
   },
+  {
+    'navarasu/onedark.nvim',
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker',
+      }
+    end,
+  },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'auto', -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = 'latte',
+          dark = 'mocha',
+        },
+        transparent_background = true, -- disables setting the background color.
+        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
+        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+        dim_inactive = {
+          enabled = false, -- dims the background color of inactive window
+          shade = 'dark',
+          percentage = 0.15, -- percentage of the shade to apply to the inactive window
+        },
+        no_italic = false, -- Force no italic
+        no_bold = false, -- Force no bold
+        no_underline = false, -- Force no underline
+        styles = { -- Handles the styles of general hi groups (see `:h highlight-args`):
+          comments = { 'italic' }, -- Change the style of comments
+          conditionals = { 'italic' },
+          loops = {},
+          functions = {},
+          keywords = {},
+          strings = {},
+          variables = {},
+          numbers = {},
+          booleans = {},
+          properties = {},
+          types = {},
+          operators = {},
+          -- miscs = {}, -- Uncomment to turn off hard-coded styles
+        },
+        color_overrides = {},
+        custom_highlights = {},
+        default_integrations = true,
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = '',
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -1049,10 +1128,6 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight'
-
-      -- vim.cmd.colorscheme 'nord'
-      vim.cmd.colorscheme 'kanagawa-dragon'
-
       -- You can configure highlights by doing something like:
       -- vim.cmd.hi 'Comment gui=none'
     end,
